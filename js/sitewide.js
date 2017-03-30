b74f8a84d9e191508eeff30a0afe2f4a1539317a
George Gu <george.zp.gu@gmail.com>
2017-03-29 23:57:20 -0400

    Added uptime counter.

--- js/sitewide.js
+++ js/sitewide.js
@@ -21,4 +21,32 @@ $('a').click(function (e) {
     setTimeout(function(){
          window.location = goTo;
     },1000);       
-}); 
+});
+
+/*
+* Basic Count Up from Date and Time
+* Author: @mrwigster / trulycode.com
+*/
+
+window.onload=function() {
+  // Month,Day,Year,Hour,Minute,Second
+  upTime('mar,29,2017,12:49:06'); // ****** Change this line!
+}
+function upTime(countTo) {
+  now = new Date();
+  countTo = new Date(countTo);
+  difference = (now-countTo);
+
+  days=Math.floor(difference/(60*60*1000*24)*1);
+  hours=Math.floor((difference%(60*60*1000*24))/(60*60*1000)*1);
+  mins=Math.floor(((difference%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);
+  secs=Math.floor((((difference%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
+
+  document.getElementById('days').firstChild.nodeValue = days;
+  document.getElementById('hours').firstChild.nodeValue = hours;
+  document.getElementById('minutes').firstChild.nodeValue = mins;
+  document.getElementById('seconds').firstChild.nodeValue = secs;
+
+  clearTimeout(upTime.to);
+  upTime.to=setTimeout(function(){ upTime(countTo); },1000);
+}
