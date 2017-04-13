86d45a7431d6aa9c0fa58f56422d3c5e2a121b30
George Gu <george.zp.gu@gmail.com>
2017-04-13 03:12:55 -0400

    First content update.

--- js/sitewide.js
+++ js/sitewide.js
@@ -1,32 +1,3 @@
-/*
-* Basic Count Up from Date and Time
-* Author: @mrwigster / trulycode.com
-*/
-
-window.onload=function() {
-  // Month,Day,Year,Hour,Minute,Second
-  upTime('mar,29,2017,12:49:06'); // ****** Change this line!
-}
-
-function upTime(countTo) {
-  now = new Date();
-  countTo = new Date(countTo);
-  difference = (now-countTo);
-
-  days=Math.floor(difference/(60*60*1000*24)*1);
-  hours=Math.floor((difference%(60*60*1000*24))/(60*60*1000)*1);
-  mins=Math.floor(((difference%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);
-  secs=Math.floor((((difference%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
-
-  document.getElementById('days').firstChild.nodeValue = days;
-  document.getElementById('hours').firstChild.nodeValue = hours;
-  document.getElementById('minutes').firstChild.nodeValue = mins;
-  document.getElementById('seconds').firstChild.nodeValue = secs;
-
-  clearTimeout(upTime.to);
-  upTime.to=setTimeout(function(){ upTime(countTo); },1000);
-}
-
 var audio = document.getElementById('init');
 
 function redirectHandler() {
@@ -38,6 +9,8 @@ audio.addEventListener('ended', redirectHandler, false);
 function play(){
 	audio.play();
 }
+
+// Animations
 
 var timeoutID;
 
@@ -53,3 +26,18 @@ function fadeOut() {
 	$("h1").removeClass("fade-in");
 	$("h1").addClass("fade-out");
 }
+
+//Text Generation
+
+function generate() {
+	var rtn = '';
+	var ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	var ID_LENGTH = 8;
+	for (var i = 0; i < ID_LENGTH; i++) {
+		rtn += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+	}
+	var lead = document.getElementById("output").innerHTML;
+	document.getElementById("output").innerHTML = lead + rtn;
+}
+
+
